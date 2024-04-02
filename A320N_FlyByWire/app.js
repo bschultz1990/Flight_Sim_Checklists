54a973843f13dd655cee1631f22f9532db7bebc1
= <=>
2024-04-02 16:14:37 -0500

    Add through taxi.

--- A320N_FlyByWire/app.js
+++ A320N_FlyByWire/app.js
@@ -1,1 +1,23 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+  const lines = document.querySelectorAll(".line");
+  const uncheckAll = document.getElementById("uncheck-all");
+  const checkAll = document.getElementById("check-all");
+
+  lines.forEach((line) => {
+    line.addEventListener("click", function () {
+      line.classList.toggle("line-complete");
+    });
+  });
+
+  uncheckAll.addEventListener("click", function () {
+    lines.forEach((line) => {
+      line.classList.remove("line-complete");
+    });
+  });
+
+  checkAll.addEventListener("click", function () {
+    lines.forEach((line) => {
+      line.classList.add("line-complete");
+    });
+  });
+});
